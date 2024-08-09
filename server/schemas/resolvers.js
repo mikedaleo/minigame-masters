c2b4c8c1653d50cd9187aa1d5d87b39693caef38
mikedaleo <mikedaleo@outlook.com>
2024-08-08 21:21:21 -0400

    removed Game model, added nodemon package, added package.json scripts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,15 +2,15 @@ const { User, Game } = require('../models'); // Import the models
 
 const resolvers = {
   Query: {
-    getUser: async (_, { id }) => {
-      return await User.findById(id); // Find user by ID
+    getUser: async (parent, { _id }) => {
+      return await User.findById(_id); // Find user by ID
     },
     getUsers: async () => {
       return await User.find(); // Get all users
     },
   },
   Mutation: {
-    createUser: async (_, { username, email, password }) => {
+    createUser: async (parent, { username, email, password }) => {
       const user = new User({ username, email, password, units: 100 });
       return await user.save(); // Create and save a new user
     },
