f165973b491ec3e46ced96392cebcc04ad96a413
Christopher Giordano <chrisgcpg@gmail.com>
2024-08-12 13:47:44 -0400

    Added styles to the components to have a basic layout.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Home() {
   return (
-        <div>
+        <div className="home">
           <section>
             <h1><b>Welcome to Minigame Masters! The site where you can earn coins playing different mini games and become master of all mini games</b></h1>
           </section>
