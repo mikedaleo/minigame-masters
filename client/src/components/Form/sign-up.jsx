d5a229868225ad537e98bfa2ec26592a52e693c1
Christopher Giordano <chrisgcpg@gmail.com>
2024-08-12 14:36:00 -0400

    Removed email from login section.

--- client/src/components/Form/sign-up.jsx
+++ client/src/components/Form/sign-up.jsx
@@ -45,7 +45,6 @@ function SignUp() {
                 <form className='form login'>
                     <h2>Login</h2>
                     <span>Login with Username</span>
-                    <input name="email" type="email" placeholder='email'></input>
                     <input name="userName" type="username" placeholder='username'></input>
                     <input name="password" type="password" placeholder='password'></input>
                     <button type="submit">Login</button>
