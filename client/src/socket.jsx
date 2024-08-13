486e7617d7a80ea346c279f5a92dbf558b4c0750
Noah Manno <noahmanno19@gmail.com>
2024-08-12 21:37:14 -0400

    new-port

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,6 +1,6 @@
 // socket.js
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001'); // Connect to backend
+const socket = io('https://minigame-masters.onrender.com'); // Connect to backend
 
 export default socket;
