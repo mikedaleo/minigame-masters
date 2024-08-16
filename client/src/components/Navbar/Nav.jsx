4a352603708e942e72066c7a975a80b3ae439b19
Noah Manno <noahmanno19@gmail.com>
2024-08-15 20:01:49 -0400

    phil's card game

--- client/src/components/Navbar/Nav.jsx
+++ client/src/components/Navbar/Nav.jsx
@@ -12,6 +12,7 @@ function Nav() {
       <div id="links">
         <Link to="/" className={isActive('/') ? 'active' : ''}>Home</Link>
         <Link to="/games" className={isActive('/games') ? 'active' : ''}>Games</Link>
+        <Link to="/single" className={isActive('/single') ? 'active' : ''}>Single Player</Link>
         <Link to="/leaderboard" className={isActive('/leaderboard') ? 'active' : ''}>Leaderboard</Link>
         <Link to="/signup" className={isActive('/signup') ? 'active' : ''}>Sign Up</Link>
         <Link to="/profile" className={isActive('/profile') ? 'active' : ''}>Profile</Link>
---
+++ client/src/pages/SinglePlayerGames.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MatchingCardGame from '../components/MatchingGame/matching-card-game'
+
+
+function SinglePlayerGames() {
+  return (
+        <MatchingCardGame />
+  )
+};
+
+export default SinglePlayerGames;
