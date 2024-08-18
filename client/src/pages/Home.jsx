cc9d32534a9226284b84ac5b4ab92082f29e8045
Noah Manno <noahmanno19@gmail.com>
2024-08-17 20:13:46 -0400

    tweaking-style

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-
 function Home() {
   return (
-        <div className="home">
-          <section>
-            <h1><b>Welcome to Minigame Masters! The site where you can earn coins playing different mini games and become master of all mini games!</b></h1>
-          </section>
-          
-
-        </div>
-  )
-};
+    <div className="home-container">
+      <section className="home-section">
+        <h1 className="home-title">
+          <b>Welcome to Minigame Masters! The site where you can earn coins playing different mini games and become master of all mini games!</b>
+        </h1>
+      </section>
+    </div>
+  );
+}
 
 export default Home;
