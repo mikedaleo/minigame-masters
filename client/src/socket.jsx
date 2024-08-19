ce5831725032baa751ca99d1e9a2b7fb6a23e4a4
mikedaleo <mikedaleo@outlook.com>
2024-08-19 18:13:28 -0400

    fixed the socket URL

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -2,6 +2,6 @@
 import io from 'socket.io-client';
 
 
-const socket = io(import.meta.env.SOCKET_URL || 'http://localhost:3001'); // Connect to backend
+const socket = io(process.env.SOCKET_URL); // Connect to backend
 
 export default socket;
